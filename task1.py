123f6f39266a340a52ac178cb7b98a2d8ac8a62f
Андрей <120337665+portu81@users.noreply.github.com>
2024-03-25 13:53:50 +0300

    Delete task1.py

--- task1.py
+++
@@ -1,8 +0,0 @@
-n = int(input())# получаем число
-res = []# создаем пустой список
-for i in range(n):# создаем условие
-    a = int(input())
-    res.append(a)# добавляем число в список
-    i += 1
-res.reverse()# переворачиваем список
-print(*res)
