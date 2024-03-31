a5bce378dc59f2cebf2e0bb51c661249ec03036a
Андрей <120337665+portu81@users.noreply.github.com>
2024-03-31 17:40:39 +0300

    Delete work9_task3.py

--- work9_task3.py
+++
@@ -1,9 +0,0 @@
-a = list(map(int, input().strip().split()))# вводим числа в строчку через пробел
-b = set()
-for i in a:
-    if i in b:
-        print('YES')
-    else:
-        print('NO')
-        b.add(i)
-#print(b)
