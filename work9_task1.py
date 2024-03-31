7dfa39ebe5da2a6717958bb06d3c70e1e892e877
Андрей <120337665+portu81@users.noreply.github.com>
2024-03-31 17:40:17 +0300

    Delete work9_task1.py

--- work9_task1.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-a = list(map(int, input().strip().split()))
-assert len(a) == n, "Количество введённых чисел не равно n"# проверяем что количество чисел равняется n
-a = set(a)
-print(len(a))
